1eb73e95f0a54ccffb948fa69156e0e755edd493
kamrul hasan <kamrul939223@gmail.com>
2024-10-31 16:16:57 +0600

    Navbar Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/frontend/Home';
+import About from './components/frontend/About';
+import './assets/css/style.scss'
 
 function App() {
  
   return (
     <>
-        <button className='btn btn-danger'>Click Here</button>
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
